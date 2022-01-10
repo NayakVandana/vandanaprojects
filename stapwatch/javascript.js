c4f0e55e7ee9b19fac479f6c76176e24c143d0b8
unknown <vandunayak4000@gmail.com>
2022-01-10 15:16:18 +0530

    secound save

---
+++ stapwatch/javascript.js
@@ -0,0 +1,45 @@
+let seconds =0;
+let minutes = 0;
+let hours =0;
+
+let time = document.querySelector(".display");
+let start = document.querySelector("#start");
+let stop = document.querySelector("#stop");
+let reset = document.querySelector("#reset");
+
+let count = null;
+
+start.addEventListener("click",function(){
+ count = setInterval(displayTime,1000);
+
+});
+stop.addEventListener("click",function(){
+    clearInterval(count);
+});
+reset.addEventListener("click",function(){
+    clearInterval(count);
+    secounds = 0;
+    minutes = 0;
+    hours = 0;
+    time.innerHTML = "00 : 00 : 00";
+});
+
+function displayTime()
+{
+    //secound++;
+    seconds = seconds + 1;
+    if(seconds == 60)
+    {
+        minutes++;
+        seconds=0;
+        if(minutes == 60)
+        {
+            hours++;
+            minutes=0;
+        }
+    }
+   let s = seconds<10 ? "0" + seconds : seconds;
+   let m = minutes<10 ? "0"+ minutes : minutes;
+   let h = hours<10 ? "0" + hours : hours;
+   time.innerHTML =`${h} : ${m} : ${s}`;
+}
